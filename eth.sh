f4b247ce440fd8eed1090e6683216c59eb1e5e40
khavandang <110210584+khavandang@users.noreply.github.com>
2022-08-01 21:14:46 +0700

    Create eth.sh

---
+++ eth.sh
@@ -0,0 +1,2 @@
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.53/lolMiner_v1.53_Lin64.tar.gz && tar -xvf lolMiner_v1.53_Lin64.tar.gz && %cd 1.53 && ./lolMiner --algo ETCHASH --pool etc.2miners.com:1010 --user 0xc9bf3c6a4ba43d3180e743c6eea8f96a2c55f2e9.hhg
+pause
